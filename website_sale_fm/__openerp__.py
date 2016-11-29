e7fce6bb8714b15d823d2dee1ce024647859bfbe
Santi Noreña <santi@punto0.org>
2016-11-29 20:39:02 +0100

    New addon to mod website_sale

---
+++ website_sale_fm/__openerp__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'eCommerce FairMarket',
+    'category': 'Website',
+    'summary': 'Modification in website_sale for FairMarke',
+    'website': 'https://www.odoo.com/page/e-commerce',
+    'version': '1.0',
+    'description': """
+OpenERP E-Commerce modifications for FairMarket
+==================
+
+    """,
+    'author': 'Punto0 - FairCoop',
+    'depends': ['website_sale'],
+    'data': [
+        #'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+        #'data/demo.xml',
+    ],
+    #'qweb': ['static/src/xml/*.xml'],
+    'installable': True,
+    'application': False,
+}
