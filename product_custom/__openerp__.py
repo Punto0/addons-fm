6ed4a9622278f7692343ff8c071ba9eb6833cd80
Santi Noreña <santi@punto0.org>
2016-12-26 13:20:29 +0100

    New module product_custom

---
+++ product_custom/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Product Custom',
+    'depends': ['product'],
+    'author': 'Santi Punto0 - FairCooop',
+    'category': 'Product',
+    'description': """
+Customization on the product model for FairMarket
+    """,
+    'website': 'https://punto0.org',
+    'data': [
+        'views/product_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
