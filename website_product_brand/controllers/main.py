ca7db95e34e31a9c005328c1afecc725dbcc29be
Santi punto0 <santi@punto0.org>
2017-10-19 12:15:05 +0200

    Read the brands controller cleaned

--- website_product_brand/controllers/main.py
+++ website_product_brand/controllers/main.py
@@ -411,6 +411,97 @@ class website_sale_extension(openerp.addons.website_sale.controllers.main.websit
               values.update({'search': post.get('search')})
           return request.website.render('website_product_brand.product_brands', values)
 
+    # Each brand page
+    @http.route(['/shop/brands'], type='http', auth='public', website=True)
+    def brands(self, page = 0, category = None, search = '', **post):
+        cr, uid, context, pool = (request.cr,
+         request.uid,
+         request.context,
+         request.registry)
+        values = {}
+        child_prod_id = []
+        domain = request.website.sale_product_domain()
+        attrib_list = request.httprequest.args.getlist('attrib')
+        attrib_values = [ map(int, v.split('-')) for v in attrib_list if v ]
+        attrib_set = set([ v[1] for v in attrib_values ])
+        if attrib_values:
+            attrib = None
+            ids = []
+            for value in attrib_values:
+                if not attrib:
+                    attrib = value[0]
+                    ids.append(value[1])
+                elif value[0] == attrib:
+                    ids.append(value[1])
+                else:
+                    domain += [('attribute_line_ids.value_ids', 'in', ids)]
+                    attrib = value[0]
+                    ids = [value[1]]
+
+            if attrib:
+                domain += [('attribute_line_ids.value_ids', 'in', ids)]
+
+        keep = QueryURL('/shop', category=category and int(category), search=search, attrib=attrib_list)
+        if not context.get('pricelist'):
+            pricelist = self.get_pricelist()
+            context['pricelist'] = int(pricelist)
+        else:
+            pricelist = pool.get('product.pricelist').browse(cr, uid, context['pricelist'], context)
+        product_obj = pool.get('product.template')
+
+       # Brand's product search
+        brand_id = False
+        if post.get('brand'):
+            product_designer_obj = pool.get('product.brand')
+            brand_ids = product_designer_obj.search(cr, SUPERUSER_ID, [('id', '=', int(post.get('brand')))])
+            domain = [('product_brand_id', 'in', brand_ids)]
+            brand_id = domain
+        url = '/shop'
+        product_count = product_obj.search_count(cr, uid, domain, context=context)
+        if search:
+            post['search'] = search
+        if category:
+            category = pool['product.public.category'].browse(cr, uid, int(category), context=context)
+            url = '/shop/category/%s' % slug(category)
+        pager = request.website.pager(url=url, total=product_count, page=page, step=PPG, scope=7, url_args=post)
+        product_ids = product_obj.search(cr,uid,domain,limit=PPG,offset=pager['offset'],order='website_published desc, website_sequence desc',context=context)
+        products = product_obj.browse(cr, uid, product_ids, context=context)
+        style_obj = pool['product.style']
+        style_ids = style_obj.search(cr, uid, [], context=context)
+        styles = style_obj.browse(cr, uid, style_ids, context=context)
+        category_obj = pool['product.public.category']
+        category_ids = category_obj.search(cr, uid, [], context=context)
+        categories = category_obj.browse(cr, uid, category_ids, context=context)
+        categs = filter(lambda x: not x.parent_id, categories)
+        attributes_obj = request.registry['product.attribute']
+        attributes_ids = attributes_obj.search(cr, uid, [], context=context)
+        attributes = attributes_obj.browse(cr, uid, attributes_ids, context=context)
+        from_currency = pool.get('product.price.type')._get_field_currency(cr, uid, 'list_price', context)
+        to_currency = pricelist.currency_id
+        compute_currency = lambda price: pool['res.currency']._compute(cr, uid, from_currency, to_currency, price, context=context)
+        brand_obj = pool.get('product.brand')
+        brand = brand_obj.browse(cr, uid, brand_ids, context=context)
+        values.update({'search': search,
+         'category': category,
+         'attrib_values': attrib_values,
+         'attrib_set': attrib_set,
+         'pager': pager,
+         'pricelist': pricelist,
+         'products': products,
+         'bins': table_compute().process(products),
+         'rows': PPR,
+         'styles': styles,
+         'categories': categs,
+         'compute_currency': compute_currency,
+         'keep': keep,
+         'style_in_product': lambda style, product: style.id in [ s.id for s in product.website_style_ids ],
+         'attributes': attributes,
+         'brand': brand,
+         'attrib_encode': lambda attribs: werkzeug.url_encode([ ('attrib', i) for i in attribs ])})
+        #return request.website.render('website_sale.brand_page', values)
+        return request.website.render('website_product_brand.brand_page', values)
+
+    # demands section
     @http.route([
         '/demands',
         '/demands/country/<model("res.country"):country>',
