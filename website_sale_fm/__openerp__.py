c2a65622c3f639446a194cac30b5204cc28347eb
Santi <santi@punto0.org>
2017-11-07 12:00:15 +0000

    Cleaning the product form

--- website_sale_fm/__openerp__.py
+++ website_sale_fm/__openerp__.py
@@ -1,7 +1,7 @@
 {
     'name': 'eCommerce FairMarket',
     'category': 'Website',
-    'summary': 'Modification in website_sale for FairMarke',
+    'summary': 'Modification in website_sale for FairMarket',
     'website': 'https://www.odoo.com/page/e-commerce',
     'version': '1.0',
     'description': """
@@ -12,7 +12,6 @@ OpenERP E-Commerce modifications for FairMarket
     'author': 'Punto0 - FairCoop',
     'depends': ['website_sale'],
     'data': [
-        'views/views.xml',
         'views/templates.xml',
     ],
     'demo': [
